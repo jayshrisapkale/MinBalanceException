f3543b58aeabb89ba6cd4439e8b15b1e9334c4b8
jayshri Sapkale <jayshri@VERINITE1.local>
2021-09-21 17:48:15 +0530

    MinBalanceException

---
+++ MinBalanceException.java
@@ -0,0 +1,37 @@
+
+
+public class MinBalanceException extends Exception {
+	
+	
+	  public MinBalanceException ()
+	  {
+	    System.out.println ("Balance is low");
+	  }
+	}
+	 class main
+	{
+	  public static void main (String[]args)
+	  {
+	    try
+	    {
+
+	      int acc[] = { 100, 101, 102, 103 }; 
+	      double balance[] = { 900, 2000, 1500, 1560 };
+	      System.out.println ("Account No" + "Balance\t");
+	      for (int i = 0; i < 4; i++)
+	   {
+	     System.out.println (acc[i] + " " + balance[i] + " ");
+	     if (balance[i] < 1000)
+	     {
+	         throw new MinBalanceException (); 
+	     }
+	   }
+	    }
+	    catch (MinBalanceException e)
+	    {
+	      System.out.println ("Exception caught");
+	    }
+	  }
+	}
+
+
